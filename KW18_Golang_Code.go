b6478ef00383ed770b824477117501da6d09df1d
iivanovicBZT <83442255+iivanovicBZT@users.noreply.github.com>
2021-05-05 19:12:20 +0200

    Update KW18_Golang_Code.go

--- KW18_Golang_Code.go
+++ KW18_Golang_Code.go
@@ -71,13 +71,13 @@ func main() {
     //Print Size, Type and Character
     fmt.Printf("Size: %d\nType: %s\nCharacter: %c\n", unsafe.Sizeof(b), reflect.TypeOf(b), b)
     
-    r := '£'
+    r := 'Â£'
     
     fmt.Println("\nPriting Rune:")
     //Print Size, Type, CodePoint and Character
     fmt.Printf("Size: %d\nType: %s\nUnicode CodePoint: %U\nCharacter: %c\n", unsafe.Sizeof(r), reflect.TypeOf(r), r, r)
 
-    s := "µ" //Micro sign
+    s := "Âµ" //Micro sign
     fmt.Println("\nPriting String:")
     fmt.Printf("Size: %d\nType: %s\nCharacter: %s\n", unsafe.Sizeof(s), reflect.TypeOf(s), s)
 }
@@ -87,22 +87,23 @@ func main() {
 //--------------------------------------------------
 //Folie 14 - Logische Datentypen (boolean)
 
- package main
- // program to perform comparison operation for the boolean data type.
- import "fmt"
- func main() {
-             var num1 = 10
-             var num2 = 12                       // variable declaration complete.
-             // use of comparison operator on the declared variable.
-             var res1 = num1 == num2
-             var res2 = num1 != num2
-             var res3 = num1 > num2
-             var res4 = num1 < num2
-             var res5 = num1 <= num2
-             var res6 = num1 >= num2
-             // all six results will store boolean value here.
-             fmt.Println(res1, res2, res3, res4, res5, res6)
- } 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// boolean operators
+	a := 3
+	b := 4
+	fmt.Println(a == b)  // false
+	fmt.Println(a != b)  // true
+	fmt.Println(a < b)   // true
+	fmt.Println(a > b)   // false
+	fmt.Println(a >= b)  // false
+	fmt.Println(a <= b)  // true
+}
 
 
 //--------------------------------------------------
@@ -119,10 +120,10 @@ func main() {
  var ErreichtePunkte float32
  const MaxPunkte float32 = 100
 
-	fmt.Println("Bitte die erreichten Punkte eintragen: (mit ENTER bestätigen)")
+	fmt.Println("Bitte die erreichten Punkte eintragen: (mit ENTER bestÃ¤tigen)")
 	fmt.Scanln(&ErreichtePunkte)
 	
-	fmt.Println("Die maximal möglichen Punkte sind:", MaxPunkte)
+	fmt.Println("Die maximal mÃ¶glichen Punkte sind:", MaxPunkte)
 	
 	var Note float32 = (ErreichtePunkte/MaxPunkte)*5+1
 	
