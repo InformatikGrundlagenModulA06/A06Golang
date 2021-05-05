273744c927fbb2415edd6728b3daae98a5e699d3
iivanovicBZT <83442255+iivanovicBZT@users.noreply.github.com>
2021-05-05 19:11:27 +0200

    Create KW18_Golang_Boolean.go

---
+++ KW18_Golang_Boolean.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// boolean operators
+	a := 3
+	b := 4
+	fmt.Println(a == b)  // false
+	fmt.Println(a != b)  // true
+	fmt.Println(a < b)   // true
+	fmt.Println(a > b)   // false
+	fmt.Println(a >= b)  // false
+	fmt.Println(a <= b)  // true
+}
